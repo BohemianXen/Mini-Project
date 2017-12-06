7b8fbbbbed220378981741f99360ca6702bbb108
BohemianXen <34287841+BohemianXen@users.noreply.github.com>
2017-12-06 00:30:25 +0000

    Added getScale function

--- Challenge#343_Easy/src/App.java
+++ Challenge#343_Easy/src/App.java
@@ -8,10 +8,48 @@
  *
  * @author blaze
  */
+import java.util.LinkedHashMap;
+
 public class App {
+    String[] notes = {"C", "C#", "D", "D#", "E", "F", "F#", "G", 
+                       "G#", "A", "A#", "B"};
+    String[] solfegeNames = {"Do", "Re", "Mi", "Fa", "So", "La", "Ti"};
+    int[] solfegeNumbers = {0, 2, 4, 5, 7, 9, 11}; 
+    LinkedHashMap <String, Integer> solfege = new LinkedHashMap<>();
     
     public void start(){
-        System.out.println("Started");
+        for (int i = 0; i < (solfegeNames).length; i++){
+            solfege.put(solfegeNames[i], solfegeNumbers[i]);
+        }
+    
+        String[] scaleIn = getScale("D");
+        for (String note: scaleIn){
+            System.out.print(note + " ");
+        }     
     }
     
+    private String[] getScale(String noteScale){
+        String[] scale = new String[solfegeNames.length];
+        int startIndex = 0;
+        for (String note: notes){
+            if (note.equals(noteScale)){ break;}
+            startIndex++;
+        }
+        
+       // if (startIndex > notes.length + 1) TODO: Add error check
+       int index, counter = 0;
+
+       for (int jump : solfegeNumbers){
+           //remainder = startIndex % notes.length;
+           index = startIndex + jump;
+           if (index >= notes.length){
+               index = notes.length - jump;
+            }
+           scale[counter] = notes[index];
+           counter++;
+           // if (counter > scale.length){break;} TODO: Handle error  
+       }
+        
+        return scale;
+    }  
 }
